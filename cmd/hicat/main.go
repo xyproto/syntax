558fe71573577cae505e0b646c715fc12b4e5bf1
Alexander F. Rødseth <rodseth@gmail.com>
2024-11-06 16:50:41 +0100

    Fix the hicat utility

--- cmd/hicat/main.go
+++ cmd/hicat/main.go
@@ -2,35 +2,46 @@ package main
 
 import (
 	"flag"
-	//"fmt"
-	"io/ioutil"
+	"io"
+	"os"
+	"strings"
 
+	"github.com/xyproto/files"
+	"github.com/xyproto/mode"
 	"github.com/xyproto/syntax"
 	"github.com/xyproto/textoutput"
 )
 
-// highlight cat
-
 func main() {
 	flag.Parse()
-
-	o := textoutput.NewTextOutput(true, true)
-
-	if flag.NArg() != 1 {
+	var (
+		o          = textoutput.NewTextOutput(true, true)
+		args       = flag.Args()
+		lenargs    = len(args)
+		inputBytes []byte
+	)
+	if lenargs != 1 {
 		o.ErrExit("Must specify exactly 1 filename argument.")
 	}
-
-	input, err := ioutil.ReadFile(flag.Arg(0))
+	if files.DataReadyOnStdin() || (lenargs > 0 && args[lenargs-1] == "-") {
+		if data, err := io.ReadAll(os.Stdin); err == nil { // success
+			inputBytes = data
+		}
+	} else { // Set msg to the given arguments (if any)
+		inputBytes = []byte(strings.Join(args, " "))
+		// If the first argument is a filename, then use that one for the input
+		if lenargs > 0 && files.Exists(args[0]) {
+			data, err := os.ReadFile(args[0])
+			if err == nil { // success
+				inputBytes = data
+			}
+		}
+	}
+	mode := mode.SimpleDetectBytes(inputBytes)
+	text, err := syntax.AsText(inputBytes, mode)
 	if err != nil {
 		o.ErrExit(err.Error())
 	}
-
-	text, err := syntax.AsText(input, false)
-	if err != nil {
-		o.ErrExit(err.Error())
-	}
-
 	textWithTags := string(text)
-	//fmt.Println(textWithTags)
 	o.OutputTags(textWithTags)
 }
