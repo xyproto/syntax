b10532773ca27c6e0c2ee42df2e8252bd09c4077
Alexander F. Rødseth <rodseth@gmail.com>
2024-11-06 17:06:31 +0100

    Update the hicat utility (a bit like bat)

--- cmd/hicat/main.go
+++ cmd/hicat/main.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 
 	"github.com/xyproto/files"
-	"github.com/xyproto/mode"
 	"github.com/xyproto/syntax"
 	"github.com/xyproto/textoutput"
 )
@@ -15,33 +14,41 @@ import (
 func main() {
 	flag.Parse()
 	var (
-		o          = textoutput.NewTextOutput(true, true)
-		args       = flag.Args()
-		lenargs    = len(args)
-		inputBytes []byte
+		o    = textoutput.NewTextOutput(true, true)
+		args = flag.Args()
 	)
-	if lenargs != 1 {
-		o.ErrExit("Must specify exactly 1 filename argument.")
-	}
-	if files.DataReadyOnStdin() || (lenargs > 0 && args[lenargs-1] == "-") {
-		if data, err := io.ReadAll(os.Stdin); err == nil { // success
-			inputBytes = data
+
+	if files.DataReadyOnStdin() { // Pri 1: read from stdin, if possible
+		inputBytes, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			o.ErrExit("Failed to read from stdin: " + err.Error())
 		}
-	} else { // Set msg to the given arguments (if any)
-		inputBytes = []byte(strings.Join(args, " "))
-		// If the first argument is a filename, then use that one for the input
-		if lenargs > 0 && files.Exists(args[0]) {
-			data, err := os.ReadFile(args[0])
-			if err == nil { // success
-				inputBytes = data
+		if err := syntax.CatBytes(inputBytes, o); err != nil {
+			o.ErrExit(err.Error())
+		}
+	} else if len(args) > 0 { // Pri 2: read from given filename(s), if possible
+		var anyFilesProcessed bool
+		for _, arg := range args {
+			if files.Exists(arg) {
+				inputBytes, err := os.ReadFile(arg)
+				if err != nil {
+					o.ErrExit("Failed to read file '" + arg + "': " + err.Error())
+				}
+				if err := syntax.CatBytes(inputBytes, o); err != nil {
+					o.ErrExit(err.Error())
+				}
+
+				anyFilesProcessed = true
 			}
 		}
+		if !anyFilesProcessed { // Pri 3: use the given arguments as the input data, as a last restort
+			inputBytes := []byte(strings.Join(args, " "))
+			if err := syntax.CatBytes(inputBytes, o); err != nil {
+				o.ErrExit(err.Error())
+			}
+		}
+	} else {
+		// No data on stdin and no arguments
+		o.ErrExit("Please provide input via stdin, given filenames or command line arguments.")
 	}
-	mode := mode.SimpleDetectBytes(inputBytes)
-	text, err := syntax.AsText(inputBytes, mode)
-	if err != nil {
-		o.ErrExit(err.Error())
-	}
-	textWithTags := string(text)
-	o.OutputTags(textWithTags)
 }
